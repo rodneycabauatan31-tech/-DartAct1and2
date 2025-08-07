a89f188a9d829c7ee3ebcbf43ee5a3c870054ee0
rodney cabautan <rodneycabauatan31@gmail.com>
2025-08-07 05:03:50 +0000

    Activity 1 and 2 : Understanding the Dart

---
+++ act1.dart
@@ -0,0 +1,8 @@
+void main() {
+  String greeting = "Welcome";
+  dynamic user = 25;
+
+  print("$greeting, $user!");
+}
+
+//if we used atring type fpr user is and tried to assign it an integer (user = 25:) dart would throw a type eror, because  string cant hold numbers
